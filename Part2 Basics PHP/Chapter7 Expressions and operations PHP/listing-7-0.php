efac5134c8046350ad90cc9482bc10cb2319e5de
PL <pavel.hr@mail.ru>
2017-01-31 12:38:09 +0300

    Глава7. Выражения и операции РНР.

---
+++ Part2 Basics PHP/Chapter7 Expressions and operations PHP/listing-7-0.php
@@ -0,0 +1,10 @@
+<?php
+// Вставка в строку элемента массива или свойства объекта
+
+    $action = [
+        "left" => "survive",
+        "right" => "kill'em all"
+    ];
+// echo "Выбранный элемент: {$action['left']}";
+// echo "Выбранный элемент: $action[left]";
+echo "Выбранный элемент: {$action[left]}";
---
+++ Part2 Basics PHP/Chapter7 Expressions and operations PHP/listing-7-10.php
@@ -0,0 +1,6 @@
+<?php
+// Оператор ??
+$x = null;
+$y = null;
+$z = 3;
+var_dump($x ?? $y ?? $z);
